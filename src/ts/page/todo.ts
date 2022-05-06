83e84613c37fa2246503b4abcdcab62a19b2812f
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-06 23:27:07 +0530

    Finalize the pending todo part

--- src/ts/page/todo.ts
+++ src/ts/page/todo.ts
@@ -1,8 +1,28 @@
-const frmTodo=document.querySelector<HTMLFormElement>("#frm-todo")!;
-const txtToDoName=document.querySelector<HTMLInputElement>("#to-do-name")!;
-const txtToDoDesc=document.querySelector<HTMLInputElement>("#to-do-desc")!;
-
-frmTodo.addEventListener('submit',(e)=>{
+const frmTodo = document.querySelector<HTMLFormElement>("#frm-todo")!;
+const txtToDoName = document.querySelector<HTMLInputElement>("#to-do-name")!;
+const txtToDoDesc = document.querySelector<HTMLInputElement>("#to-do-desc")!;
+const tblPending = document.querySelector<HTMLTableElement>("table")!;
+frmTodo.addEventListener('submit', (e) => {
     e.preventDefault();
-   console.log('submitted');
+    let rowElm = document.createElement("tr");
+    let html = `
+            <td ><span class="material-symbols-outlined done">
+                done
+            </span></td>
+            <td>${txtToDoName.value}</td>
+            <td>${txtToDoDesc.value}</td>
+ `;
+    rowElm.innerHTML = html;
+    tblPending.querySelector("tbody")!.append(rowElm);
+});
+
+tblPending.querySelector("tbody")!.addEventListener('click', (e) => {
+    if ((e.target as HTMLElement).classList.contains('done')) {
+        const elm = e.target as HTMLElement;
+        const row = elm.closest<HTMLTableRowElement>('tr')!;
+        console.log(elm);
+        console.log(row);
+        row.remove();
+0
+    }
 });
