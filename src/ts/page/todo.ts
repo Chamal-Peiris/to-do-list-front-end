3792b1e7420645f92f2331b8c08b14b12617a165
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-07 00:09:39 +0530

    Finalize the completed todo part

--- src/ts/page/todo.ts
+++ src/ts/page/todo.ts
@@ -1,7 +1,8 @@
 const frmTodo = document.querySelector<HTMLFormElement>("#frm-todo")!;
 const txtToDoName = document.querySelector<HTMLInputElement>("#to-do-name")!;
 const txtToDoDesc = document.querySelector<HTMLInputElement>("#to-do-desc")!;
-const tblPending = document.querySelector<HTMLTableElement>("table")!;
+const tblPending = document.querySelector<HTMLTableElement>("#tbl-pending")!;
+const tblCompleted= document.querySelector<HTMLTableElement>("#tbl-completed")!;
 frmTodo.addEventListener('submit', (e) => {
     e.preventDefault();
     let rowElm = document.createElement("tr");
@@ -14,8 +15,11 @@ frmTodo.addEventListener('submit', (e) => {
  `;
     rowElm.innerHTML = html;
     tblPending.querySelector("tbody")!.append(rowElm);
+    frmTodo.reset();
 });
-
+// <span class="material-symbols-outlined">
+//     close
+//     </span>
 tblPending.querySelector("tbody")!.addEventListener('click', (e) => {
     if ((e.target as HTMLElement).classList.contains('done')) {
         const elm = e.target as HTMLElement;
@@ -23,6 +27,23 @@ tblPending.querySelector("tbody")!.addEventListener('click', (e) => {
         console.log(elm);
         console.log(row);
         row.remove();
-0
+        row.querySelector("span")!.classList.remove('done');
+        row.querySelector("span")!.classList.add('remove');
+        row.querySelector("span")!.innerText='close';
+        tblCompleted.querySelector("tbody")!.append(row) ;  //appending  to completed
+    }
+});
+
+tblCompleted.querySelector("tbody")!.addEventListener('click',(e)=>{
+    if ((e.target as HTMLElement).classList.contains('remove')) {
+        const elm = e.target as HTMLElement;
+        const row = elm.closest<HTMLTableRowElement>('tr')!;
+        console.log(elm);
+        console.log(row);
+        row.remove();
+        row.querySelector("span")!.classList.remove('remove');
+        row.querySelector("span")!.classList.add('done');
+        row.querySelector("span")!.innerText='done';
+        tblPending.querySelector("tbody")!.append(row) ;  //appending  to completed
     }
 });
